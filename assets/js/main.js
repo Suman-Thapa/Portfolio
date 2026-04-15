9c741ca4f7271d86ae0748f833d2b685e6a2a912
Suman-Thapa <sumanthapa302702@gmail.com>
2026-04-15 13:46:47 +0545

    Changing the ui

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,11 +122,6 @@ const icon = document.getElementById("themeIcon");
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 
-  if (document.body.classList.contains("dark")) {
-    icon.classList.remove("bx-moon");
-    icon.classList.add("bx-sun");
-  } else {
-    icon.classList.remove("bx-sun");
-    icon.classList.add("bx-moon");
-  }
+  icon.classList.toggle("bx-moon");
+  icon.classList.toggle("bx-sun");
 });
